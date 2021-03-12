a6c9e5deccfbf725858449f4e4fec38c87cef42c
Michael Kreil <github@michael-kreil.de>
2021-03-12 15:06:30 +0100

    close #3

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ const blockchainPlus = require('blockchain-plus');
 blockchainPlus(1000); // <- creates 1000 blockchains!!!
 ```
 
+## Test suite
+
 The library even comes with a fully fledged test suite!
 
 Yes, you have richtig gelesen! Watch and staune:
@@ -26,3 +28,28 @@ Yes, you have richtig gelesen! Watch and staune:
 Yes, it works! 🥦
 (verified with a high security certification broccoli)
 ```
+
+## We put AI in blockch-AI-n
+
+Since version 2.0, each blockchain also contains an AI. These AIs are neural networks (single-layer perceptrons) that have been individually trained on exceptionally entropic data.
+
+```js
+console.log(blockchains[0])
+[
+	…
+  '16eca9e6e1ee06574b770345f92e0daa1632400f1b14efe57bc36818c80fcd11',
+  …
+  AI: {
+    fit: [Function],
+    transform: [Function],
+    fit_transform: [Function],
+    fit_random: [Function],
+    error: [Function],
+    learn: [Function]
+  }
+]
+```
+
+
+
+
--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function createBlockchain() {
 	addBlock('Maybe you have been verarscht by high staplers?');
 	addBlock('And the people have to pay now for this Unsinn with their Steuergelder?');
 
+	blockchain.AI = createAI().fit_random();
+
 	blockchains.push(blockchain);
 
 	function addBlock(message) {
@@ -28,5 +30,34 @@ function createBlockchain() {
 
 	function getHash(text) {
 		return crypto.createHash('sha256').update(''+text).digest('hex');
+	}
+
+	function createAI() {
+		// adding single-layer perceptron 
+		let weights = [0,0];
+		let perceptron = {
+			fit:(x,y) => {
+				for (let i = 0; i < 1e3; i++) perceptron.learn(x,y);
+				return perceptron;
+			},
+			transform:(x) => x.map(v => f(v*weights[0] + weights[1])),
+			fit_transform:(x,y) => perceptron.fit(x,y).transform(x),
+			fit_random:() => {
+				let x = (new Array(100)).fill(0).map(v => rand());
+				let y = x.map(v => rand());
+				return perceptron.fit(x,y);
+			},
+			error:(x,y) => perceptron.transform(x).reduce((s,v,i) => s+Math.pow(v-y[i], 2), 0),
+			learn:(x,y) => {
+				// let's call this crap "Simulated Annealing", ok?
+				let oldWeights = weights, error0 = perceptron.error(x,y);
+				weights = weights.map(v => v + rand());
+				if (perceptron.error(x,y) > error0) weights = oldWeights;
+			}
+		}
+		return perceptron;
+
+		function f(v) { return (v < 0) ? 0 : 1 }
+		function rand() { return Math.tan(Math.random()*3-1.5) }
 	}
 }
